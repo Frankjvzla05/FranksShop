a4ccd5513b550e91d188424012900758b104d7c0
Francisco M <fmaduro@ecomsur.com>
2024-03-19 23:08:30 -0300

    mejoras proyecto ecommerce

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let cantidadProductosEnCarrito = 0;
 let montoTotal = 0;
+let descuento = 0;
 const productosEnCarrito = [];
 
 const promoDescuento = 5;
@@ -9,23 +10,23 @@ function agregarAlCarrito(nombre, precio) {
     cantidadProductosEnCarrito += 1;
     montoTotal += precio;
     productosEnCarrito.push({ nombre: nombre, precio: precio });
-    actualizarCarrito();
+    actualizarCarrito(montoTotal);
 }
 
 function eliminarDelCarrito(index) {
     montoTotal -= productosEnCarrito[index].precio;
     productosEnCarrito.splice(index, 1);
     cantidadProductosEnCarrito -= 1;
-    actualizarCarrito();
+    actualizarCarrito(montoTotal);
 }
 
-function actualizarCarrito() {
+function actualizarCarrito(montoTotal) {
     console.log("Cantidad de productos:", cantidadProductosEnCarrito);
     console.log("Monto total: $", montoTotal.toFixed(2));
-   
+
     document.getElementById("cantidadProductosEnCarrito").textContent = cantidadProductosEnCarrito;
     document.getElementById("montoTotal").textContent = montoTotal.toFixed(2);
-   
+
     const productosCarrito = document.getElementById("productosCarrito");
     productosCarrito.innerHTML = "";
 
@@ -40,8 +41,9 @@ function actualizarCarrito() {
     });
 
     if (cantidadProductosEnCarrito >= promoDescuento) {
-        const descuento = (montoTotal * porcentajeDescuento) / 100;
+        descuento = (montoTotal * porcentajeDescuento) / 100;
         montoTotal -= descuento;
+        console.log("Monto con descuento: $", montoTotal.toFixed(2));
         document.getElementById("descuentoMensaje").textContent = `¡Felicidades! Se aplicó un ${porcentajeDescuento}% de descuento. Descuento aplicado: $${descuento.toFixed(2)}. Nuevo monto total: $${montoTotal.toFixed(2)}`;
     } else {
         document.getElementById("descuentoMensaje").textContent = "No se aplicó ningún descuento.";
