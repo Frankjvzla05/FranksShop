c48a4b40e4c7543255ab86b40513f9b8517fa330
Francisco M <fmaduro@ecomsur.com>
2024-04-03 23:02:18 -0300

    mejoras pre entrega 3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ let cantidadProductosEnCarrito = 0;
 let montoTotal = 0;
 let descuento = 0;
 const productosEnCarrito = [];
-
+window.sessionStorage.setItem('productQuantity', cantidadProductosEnCarrito);
+window.sessionStorage.setItem('productsInCart', productosEnCarrito);
 const promoDescuento = 5;
 const porcentajeDescuento = 10;
 
@@ -11,6 +12,11 @@ function agregarAlCarrito(nombre, precio) {
     montoTotal += precio;
     productosEnCarrito.push({ nombre: nombre, precio: precio });
     actualizarCarrito(montoTotal);
+    actualizarCantidadProductosEnCarrito(cantidadProductosEnCarrito);
+}
+function actualizarCantidadProductosEnCarrito(cantidad) {
+    document.getElementById("cantidadProductosEnCarrito").textContent = cantidad;
+    window.sessionStorage.setItem('productQuantity', cantidad);
 }
 
 function eliminarDelCarrito(index) {
@@ -18,6 +24,7 @@ function eliminarDelCarrito(index) {
     productosEnCarrito.splice(index, 1);
     cantidadProductosEnCarrito -= 1;
     actualizarCarrito(montoTotal);
+    actualizarCantidadProductosEnCarrito(cantidadProductosEnCarrito);
 }
 
 function actualizarCarrito(montoTotal) {
@@ -54,3 +61,48 @@ function actualizarCarrito(montoTotal) {
 
     document.getElementById("montoTotal").textContent = montoTotal.toFixed(2);
 }
+
+const createProduct = (product) => {
+    const producto = document.createElement("div");
+    producto.setAttribute("class", "producto");
+    const productImage = document.createElement("img");
+    productImage.setAttribute('src', `${product.imagen}`);
+    producto.appendChild(productImage);
+    const productName = document.createElement("h3");
+    const productNameText = document.createTextNode(`${product.nombre}`);
+    productName.appendChild(productNameText);
+    producto.appendChild(productName);
+
+    const productPrice = document.createElement("p");
+    const productPriceText = document.createTextNode(`$${product.precio}`);
+    productPrice.appendChild(productPriceText);
+    producto.appendChild(productPrice);
+
+    const addToCartButton = document.createElement("button");
+    const addToCartButtonText = document.createTextNode("Añadir al carro");
+    addToCartButton.appendChild(addToCartButtonText);
+    addToCartButton.setAttribute("onClick", `agregarAlCarrito('${product.nombre}' , ${product.precio})`)
+    producto.appendChild(addToCartButton);
+
+    const container = document.getElementById('productos');
+    container.appendChild(producto);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('/data/productos.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            data.map(product => {
+                return (createProduct(product))
+            });
+        })
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cantidadProductosEnCarrito = window.sessionStorage.getItem('productQuantity') || 0;
+
+    const cantidadProductosP = document.querySelector('.cantidadProductosEnCarritoIcono');
+    cantidadProductosP.textContent = cantidadProductosEnCarrito;
+});
